9b58051a7bbf7d07584a92171e12a3ef2fb981d6
vitalii boiko <vukelan@gmail.com>
2025-07-15 19:24:58 +0300

    add play screen stub

---
+++ lib/src/play.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class PlayScreen extends StatefulWidget {
+  const PlayScreen({super.key});
+  final String title = 'play';
+  @override
+  State<PlayScreen> createState() => _PlayScreenState();
+}
+
+class _PlayScreenState extends State<PlayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextButton(onPressed: () {}, child: Text('Start')),
+          ],
+        ),
+      ),
+    );
+  }
+}
