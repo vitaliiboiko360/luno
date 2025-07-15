5d370af80ad4c14b810e21ab15c2005f2809da9e
vitalii boiko <vukelan@gmail.com>
2025-07-15 19:24:43 +0300

    add style palette

---
+++ lib/style/color_palette.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  Color get pen => const Color(0xff1d75fb);
+  Color get darkPen => const Color(0xFF0050bc);
+  Color get redPen => const Color(0xFFd10841);
+  Color get inkFullOpacity => const Color(0xff352b42);
+  Color get ink => const Color(0xee352b42);
+  Color get accept => const Color(0xff15a44d);
+  Color get backgroundMain => const Color(0xffffd7ff);
+  Color get backgroundLevelSelection => const Color(0xffa2dcc7);
+  Color get backgroundPlaySession => const Color(0xffffebb5);
+  Color get background4 => const Color(0xffffffd1);
+  Color get backgroundSettings => const Color(0xffbfc8e3);
+  Color get trueWhite => const Color(0xffffffff);
+}
