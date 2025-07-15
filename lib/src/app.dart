2ca80006180436e4325d9fef9d12bb5542c0dc26
vitalii boiko <vukelan@gmail.com>
2025-07-15 19:24:04 +0300

    wrap app lifecycle observerg

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:luno/src/home.dart';
+import 'package:luno/app_lifecycle/app_lifecycle.dart';
+import 'package:luno/style/color_palette.dart';
+import 'package:provider/provider.dart';
+import 'package:luno/router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Uno',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return AppLifecycleObserver(
+      child: MultiProvider(
+        providers: [Provider(create: (context) => Palette())],
+        child: Builder(
+          builder: (context) {
+            final palette = context.watch<Palette>();
+            return MaterialApp.router(
+              title: 'Uno Game',
+              theme:
+                  ThemeData.from(
+                    colorScheme: ColorScheme.fromSeed(
+                      seedColor: palette.darkPen,
+                      surface: palette.backgroundMain,
+                    ),
+                    textTheme: TextTheme(
+                      bodyMedium: TextStyle(color: palette.ink),
+                    ),
+                    useMaterial3: true,
+                  ).copyWith(
+                    filledButtonTheme: FilledButtonThemeData(
+                      style: FilledButton.styleFrom(
+                        textStyle: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+              routerConfig: router,
+            );
+          },
+        ),
       ),
-      home: const HomePage(title: 'Start Page'),
     );
   }
 }
