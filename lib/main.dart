4326bb1ecfa57003421628791fe473988134d103
vitalii boiko <vukelan@gmail.com>
2025-07-15 22:16:47 +0300

    add logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:luno/src/app.dart';
+import 'package:logging/logging.dart';
+import 'dart:developer' as dev;
 
 void main() {
+  Logger.root.level = kDebugMode ? Level.FINE : Level.INFO;
+  Logger.root.onRecord.listen((record) {
+    dev.log(
+      record.message,
+      time: record.time,
+      level: record.level.value,
+      name: record.loggerName,
+    );
+  });
+
   runApp(const App());
 }
