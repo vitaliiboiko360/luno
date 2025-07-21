1d88b2331d3950c71dfc248a4ee93b6a6de03136
vitalii boiko <vukelan@gmail.com>
2025-07-22 00:39:08 +0300

    play page as stack

--- lib/play/play.dart
+++ lib/play/play.dart
@@ -14,25 +14,41 @@ class _PlayScreenState extends State<PlayScreen> {
   @override
   Widget build(BuildContext context) {
     final palette = context.watch<Palette>();
+    double width = MediaQuery.sizeOf(context).width;
+    double height = MediaQuery.sizeOf(context).height;
+    double midWidth = width / 2;
+
+    var padding = MediaQuery.paddingOf(context);
+    double safeHeight = height - padding.top - padding.bottom;
+    double midHeight = safeHeight / 2;
 
     return MediaQuery.withNoTextScaling(
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextButton(
-                onPressed: () {
-                  pixiInit();
-                },
-                child: Text('Init'),
+        body: SizedBox(
+          width: width,
+          height: safeHeight,
+          child: Stack(
+            children: [
+              Positioned(
+                left: midWidth,
+                top: midHeight,
+                child: TextButton(
+                  onPressed: () {
+                    pixiInit();
+                  },
+                  child: Text('Init'),
+                ),
               ),
-              TextButton(
-                onPressed: () {
-                  startStopMoving();
-                },
-                child: Text('Play'),
+              Positioned(
+                left: midWidth,
+                top: midHeight + 40,
+                child: TextButton(
+                  onPressed: () {
+                    startStopMoving();
+                  },
+                  child: Text('Play'),
+                ),
               ),
             ],
           ),
