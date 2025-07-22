ba62fc7d2fd2facfd58debf2ab559fef369b90b3
vitalii boiko <vukelan@gmail.com>
2025-07-22 16:52:20 +0300

    add svg component card

--- lib/play/card.dart
+++ lib/play/card.dart
@@ -2,14 +2,15 @@ import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/geometry.dart';
 import 'package:flame/extensions.dart';
+import 'package:flame_svg/flame_svg.dart';
 
-class UnoCard extends SpriteComponent with TapCallbacks {
+class UnoCard extends SvgComponent with TapCallbacks {
   UnoCard({super.position})
     : super(size: Vector2.all(200), anchor: Anchor.center);
 
   @override
   Future<void> onLoad() async {
-    sprite = await Sprite.load('ast/UNO_black_background_UnoCard_back.svg');
+    svg = await Svg.load('cards/UNO_black_background_card_back.svg');
   }
 
   @override
