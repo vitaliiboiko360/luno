80ee95597d8bebf918a50652d481242b5d91bc8e
vitalii boiko <vukelan@gmail.com>
2025-07-23 04:01:35 +0300

    add to the world

--- lib/play/uno_world.dart
+++ lib/play/uno_world.dart
@@ -5,6 +5,7 @@ import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:luno/play/card.dart';
+import 'package:luno/play/player_box.dart';
 
 class UnoWorld extends World {
   UnoWorld({super.key});
@@ -12,5 +13,9 @@ class UnoWorld extends World {
   @override
   FutureOr<void> onLoad() async {
     add(UnoCard(position: Vector2(0, 0)));
+    add(SvgPicture(PlayerSeat.left));
+    add(SvgPicture(PlayerSeat.top));
+    add(SvgPicture(PlayerSeat.right));
+    add(SvgPicture(PlayerSeat.mainSeat));
   }
 }
