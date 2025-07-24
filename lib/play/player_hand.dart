5eb36b9b5443fcc8591e86bbe14580f87cb48605
vitalii boiko <vukelan@gmail.com>
2025-07-24 16:48:00 +0300

    add card alignment

--- lib/play/player_hand.dart
+++ lib/play/player_hand.dart
@@ -7,18 +7,97 @@ const width = 246;
 const height = 362;
 const scale = 0.42;
 
+enum CardPlace {
+  left,
+  beforeLeft,
+  centerLeft,
+  center,
+  centerRight,
+  beforeRight,
+  right,
+}
+
 class UnoCardHand extends SvgComponent {
-  UnoCardHand(Vector2 position)
+  UnoCardHand(Vector2 position, this.cardPlace)
     : super(
         scale: Vector2.all(1),
         anchor: Anchor.center,
-        size: Vector2(width / 10 * 3, height / 10 * 3),
+        size: Vector2(width / 10 * 2.75, height / 10 * 2.75),
         position: position,
       );
+
+  CardPlace cardPlace;
+
+  setCardAngle() {
+    if (cardPlace == CardPlace.left) {
+      angle = 0.3;
+    }
+    if (cardPlace == CardPlace.beforeLeft) {
+      angle = 0.2;
+    }
+    if (cardPlace == CardPlace.centerLeft) {
+      angle = 0.1;
+    }
+    if (cardPlace == CardPlace.center) {
+      angle = 0;
+    }
+    if (cardPlace == CardPlace.centerRight) {
+      angle = -0.1;
+    }
+    if (cardPlace == CardPlace.beforeRight) {
+      angle = -0.2;
+    }
+    if (cardPlace == CardPlace.right) {
+      angle = -0.3;
+    }
+  }
 
   @override
   Future<void> onLoad() async {
     svg = await Svg.load('cards/UNO_black_background_card_back.svg');
+    setCardAngle();
+  }
+}
+
+class UnoCardHandRight extends SvgComponent {
+  UnoCardHandRight(Vector2 position, this.cardPlace)
+    : super(
+        scale: Vector2.all(1),
+        anchor: Anchor.center,
+        size: Vector2(width / 10 * 2.75, height / 10 * 2.75),
+        position: position,
+      );
+
+  CardPlace cardPlace;
+
+  setCardAngle() {
+    if (cardPlace == CardPlace.left) {
+      angle = 0.4;
+    }
+    if (cardPlace == CardPlace.beforeLeft) {
+      angle = 0.3;
+    }
+    if (cardPlace == CardPlace.centerLeft) {
+      angle = 0.2;
+    }
+    if (cardPlace == CardPlace.center) {
+      angle = 0.1;
+    }
+    if (cardPlace == CardPlace.centerRight) {
+      angle = 0;
+    }
+    if (cardPlace == CardPlace.beforeRight) {
+      angle = -0.1;
+    }
+    if (cardPlace == CardPlace.right) {
+      angle = -0.2;
+    }
+  }
+
+  @override
+  Future<void> onLoad() async {
+    svg = await Svg.load('cards/UNO_black_background_card_back.svg');
+    setCardAngle();
   }
 }
 
@@ -27,13 +106,13 @@ class PlayerHand extends PositionComponent {
 
   @override
   FutureOr<void> onLoad() {
-    add(UnoCardHand(Vector2(60, 0)));
-    add(UnoCardHand(Vector2(50, 0)));
-    add(UnoCardHand(Vector2(40, 0)));
-    add(UnoCardHand(Vector2(30, 0)));
-    add(UnoCardHand(Vector2(20, 0)));
-    add(UnoCardHand(Vector2(10, 0)));
-    add(UnoCardHand(Vector2(0, 0)));
+    add(UnoCardHand(Vector2(60, 3), CardPlace.left));
+    add(UnoCardHand(Vector2(50, 1), CardPlace.beforeLeft));
+    add(UnoCardHand(Vector2(40, 0), CardPlace.centerLeft));
+    add(UnoCardHand(Vector2(30, 1), CardPlace.center));
+    add(UnoCardHand(Vector2(20, 3), CardPlace.centerRight));
+    add(UnoCardHand(Vector2(10, 5), CardPlace.beforeRight));
+    add(UnoCardHand(Vector2(0, 7), CardPlace.right));
     return super.onLoad();
   }
 }
@@ -43,13 +122,13 @@ class PlayerHandRight extends PositionComponent {
 
   @override
   FutureOr<void> onLoad() {
-    add(UnoCardHand(Vector2(0, 0)));
-    add(UnoCardHand(Vector2(10, 0)));
-    add(UnoCardHand(Vector2(20, 0)));
-    add(UnoCardHand(Vector2(30, 0)));
-    add(UnoCardHand(Vector2(40, 0)));
-    add(UnoCardHand(Vector2(50, 0)));
-    add(UnoCardHand(Vector2(60, 0)));
+    add(UnoCardHand(Vector2(0, 5), CardPlace.right));
+    add(UnoCardHand(Vector2(10, 3), CardPlace.beforeRight));
+    add(UnoCardHand(Vector2(20, 1), CardPlace.centerRight));
+    add(UnoCardHand(Vector2(30, 0), CardPlace.center));
+    add(UnoCardHand(Vector2(40, 1), CardPlace.centerLeft));
+    add(UnoCardHand(Vector2(50, 3), CardPlace.beforeLeft));
+    add(UnoCardHand(Vector2(60, 5), CardPlace.left));
     return super.onLoad();
   }
 }
