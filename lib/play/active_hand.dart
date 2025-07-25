688eaeec9e954bf769d703d3f2a0174c43113082
vitalii boiko <vukelan@gmail.com>
2025-07-25 13:44:18 +0300

    adjust position

--- lib/play/active_hand.dart
+++ lib/play/active_hand.dart
@@ -3,14 +3,14 @@ import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:luno/play/playing_card.dart';
 
-class ActiveHand extends PositionComponent {
-  ActiveHand() : super(anchor: Anchor.center, position: Vector2(0, 0));
+class ActiveHand extends Component {
+  ActiveHand() : super();
 
   @override
   FutureOr<void> onLoad() {
-    add(PlayingCard(Vector2(0, 0)));
-    add(PlayingCard(Vector2(10, 0)));
-    add(PlayingCard(Vector2(20, 0)));
+    add(PlayingCard(Vector2(-150, 0)));
+    add(PlayingCard(Vector2(-100, 0)));
+    add(PlayingCard(Vector2(-50, 0)));
     return super.onLoad();
   }
 }
