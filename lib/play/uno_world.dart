e6113b98977da132452bb91a414b865438c36eb8
vitalii boiko <vukelan@gmail.com>
2025-07-28 23:47:07 +0300

    add and test opponent move

--- lib/play/uno_world.dart
+++ lib/play/uno_world.dart
@@ -7,6 +7,7 @@ import 'package:flutter/widgets.dart';
 import 'package:luno/play/active_hand.dart';
 import 'package:luno/play/card.dart';
 import 'package:luno/play/deck_cards.dart';
+import 'package:luno/play/move_opp_card.dart';
 import 'package:luno/play/player_box.dart';
 
 class UnoWorld extends World {
@@ -14,12 +15,28 @@ class UnoWorld extends World {
 
   @override
   FutureOr<void> onLoad() async {
-    add(UnoCard(position: Vector2(0, 0)));
+    // add(UnoCard(position: Vector2(0, 0)));
     add(PlayerBox(PlayerSeat.left));
     add(PlayerBox(PlayerSeat.top));
     add(PlayerBox(PlayerSeat.right));
     add(ActiveHand());
     add(MainPlayerBox());
     add(DeckCardsPositioned());
+  }
+
+  double deltaTime = 0;
+  bool added = false;
+
+  @override
+  void update(double dt) {
+    if (added == false) {
+      deltaTime += dt;
+      if (deltaTime > 3) {
+        added = true;
+        deltaTime = 0;
+        add(LeftMove());
+      }
+    }
+    super.update(dt);
   }
 }
