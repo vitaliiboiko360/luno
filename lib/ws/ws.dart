f0bad397f01a479481f569874f553bc91b2fc835
vitalii boiko <vukelan@gmail.com>
2025-08-07 22:25:55 +0300

    remove notifer base class

--- lib/ws/ws.dart
+++ lib/ws/ws.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/status.dart' as status;
 
-class Ws with ChangeNotifier {
+class Ws {
   final wsUrl = Uri.parse('ws://192.168.0.101:5290/ws');
 
   late var channel;
@@ -17,7 +17,7 @@ class Ws with ChangeNotifier {
       isValid = true;
       channel.stream.listen(onMessage, onError: onError, onDone: onDone);
     } catch (e) {
-      print('channel failed to be ready');
+      print('\n\nchannel failed to be ready ${e.toString()}\n');
     }
   }
 
@@ -33,24 +33,24 @@ class Ws with ChangeNotifier {
   void onMessage(message) {
     print(message);
     _message = message;
-    notifyListeners();
+    // notifyListeners();
   }
 
   dynamic getMessage() {
     return _message;
   }
 
-  void onError() {
-    print('websocket error');
+  void onError(error) {
+    print('websocket error: $error');
   }
 
   void onDone() {
     print('websocket is closing');
   }
 
-  @override
-  void dispose() {
-    channel.sink.close(status.goingAway);
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   channel.sink.close(status.goingAway);
+  //   super.dispose();
+  // }
 }
