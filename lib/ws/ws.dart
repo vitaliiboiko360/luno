4c00e8eec16e57d7540603f09d5bba603010874a
vitalii boiko <vukelan@gmail.com>
2025-08-07 18:21:38 +0300

    add logging to ws

--- lib/ws/ws.dart
+++ lib/ws/ws.dart
@@ -8,20 +8,36 @@ class Ws with ChangeNotifier {
   final wsUrl = Uri.parse('ws://192.168.0.101:5290/ws');
 
   late var channel;
+  late var isValid = false;
 
   void connect() async {
     channel = WebSocketChannel.connect(wsUrl);
-    await channel.ready;
-    channel.stream.listen(onMessage, onError: onError, onDone: onDone);
+    try {
+      await channel.ready;
+      isValid = true;
+      channel.stream.listen(onMessage, onError: onError, onDone: onDone);
+    } catch (e) {
+      print('channel failed to be ready');
+    }
   }
 
   void send(ByteBuffer buffer) async {
+    if (!isValid) {
+      print('trying to send $buffer but WS is not valid');
+    }
     channel.sink.add(buffer);
   }
 
+  var _message;
+
   void onMessage(message) {
     print(message);
+    _message = message;
     notifyListeners();
+  }
+
+  dynamic getMessage() {
+    return _message;
   }
 
   void onError() {
