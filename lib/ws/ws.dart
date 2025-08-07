70ae5083c2723df0139305d6fb80055f2b410b03
vitalii boiko <vukelan@gmail.com>
2025-08-07 03:31:32 +0300

    add simple handlers to ws

--- lib/ws/ws.dart
+++ lib/ws/ws.dart
@@ -1,15 +1,35 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/status.dart' as status;
 
 class Ws with ChangeNotifier {
-  final wsUrl = Uri.parse('ws://uno-game.ddns.net');
+  final wsUrl = Uri.parse('ws://192.168.0.101:5290/ws');
 
   late var channel;
 
   void connect() async {
     channel = WebSocketChannel.connect(wsUrl);
     await channel.ready;
+    channel.stream.listen(onMessage, onError: onError, onDone: onDone);
+  }
+
+  void send(ByteBuffer buffer) async {
+    channel.sink.add(buffer);
+  }
+
+  void onMessage(message) {
+    print(message);
+    notifyListeners();
+  }
+
+  void onError() {
+    print('websocket error');
+  }
+
+  void onDone() {
+    print('websocket is closing');
   }
 
   @override
