7f167139e32fa99c93979c4baae0fd87d4960f49
vitalii boiko <vukelan@gmail.com>
2025-08-13 23:47:14 +0300

    add send to ws object

--- web/script.js
+++ web/script.js
@@ -1,9 +1,15 @@
 (function () {
   class WsService {
-    static wsUrl = 'wss://uno-game.ddns.com:7190/ws';
+    static wsUrl = 'wss://uno-game.ddns.com:7192/ws';
     ws;
     constructor() {
       ws = new WebSocket(wsUrl);
     }
+
+    send(arrayBuffer) {
+      this.ws.send(arrayBuffer);
+    }
   }
+
+  window.ws = new WsService();
 })();
