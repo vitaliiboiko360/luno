843389ff32d7045f0ce6fd8cfc5d089e51fe6ff0
vitalii boiko <vukelan@gmail.com>
2025-08-15 01:26:41 +0300

    add processing ws message

--- lib/ws/ws.dart
+++ lib/ws/ws.dart
@@ -2,6 +2,21 @@
 library;
 
 import 'dart:js_interop';
+import 'dart:typed_data';
 
 @JS('window.ws.send')
 external void wsSend(JSArrayBuffer arrayBuffer);
+
+@JS()
+external set wsOnMessage(JSFunction function);
+
+void processWsMessage(JSArrayBuffer arrayBuffer) {
+  print(
+    Uint8List.view(arrayBuffer.toDart).fold<String>("recieved: ", (
+      str,
+      number,
+    ) {
+      return '$str $number.toString()';
+    }),
+  );
+}
