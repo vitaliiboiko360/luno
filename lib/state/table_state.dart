e9970adf0ac5c6cb025094d71f859408a88317c0
vitalii boiko <vukelan@gmail.com>
2025-08-20 15:28:46 +0300

    make pure state class

--- lib/state/table_state.dart
+++ lib/state/table_state.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 
 enum Seat { unassigned, top, right, bottom, left }
 
-class TableState extends ChangeNotifier {
+class TableState {
   Seat _seat = Seat.unassigned;
 
   void takeSeat(Seat seat) {
     _seat = seat;
-    notifyListeners();
   }
 }
