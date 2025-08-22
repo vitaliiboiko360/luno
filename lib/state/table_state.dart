a5d3f17e0d71d6a7f8818f1dd13b288e819fd5f4
vitalii boiko <vukelan@gmail.com>
2025-08-22 22:12:31 +0300

    add table state to manager

--- lib/state/table_state.dart
+++ lib/state/table_state.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:luno/state/table_game_manager.dart';
 
 enum Seat { unassigned, top, right, bottom, left }
 
 class TableState {
+  TableState(this.tgm);
+  TableGameManager tgm;
+
   Seat _seat = Seat.unassigned;
 
   void takeSeat(Seat seat) {
