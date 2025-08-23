20488d74ba689f6539b827613daddaf34adbe08b
vitalii boiko <vukelan@gmail.com>
2025-08-23 22:38:39 +0300

    process message

--- lib/state/table_state.dart
+++ lib/state/table_state.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:typed_data';
 import 'package:luno/state/table_game_manager.dart';
 
 enum Seat { unassigned, top, right, bottom, left }
+
+const seatGrantedOffset = 4;
 
 class TableState {
   TableState(this.tgm);
   TableGameManager tgm;
 
   Seat _seat = Seat.unassigned;
+
+  void processMessage(Uint8List messageByteArray) {
+    messageByteArray[seatGrantedOffset];
+  }
 
   void takeSeat(Seat seat) {
     _seat = seat;
