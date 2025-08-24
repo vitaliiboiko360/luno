f9b4515f3a02a286acddb4166b0d919d40149468
vitalii boiko <vukelan@gmail.com>
2025-08-24 21:10:33 +0300

    process seat all

--- lib/state/table_state.dart
+++ lib/state/table_state.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:luno/state/event_names.dart';
 import 'dart:typed_data';
 import 'package:luno/state/table_game_manager.dart';
-
-enum Seat { unassigned, top, right, bottom, left }
 
 const seatGrantedOffset = 2;
 
@@ -13,11 +12,55 @@ class TableState {
   Seat _seat = Seat.unassigned;
 
   void processMessage(Uint8List messageByteArray) {
-    var seatNumber = messageByteArray[seatGrantedOffset];
-    tgm.update('seat', seatNumber);
+    var action = messageByteArray[actionByteIndex];
+    if (action == SeatGranted) {
+      var seatNumber = messageByteArray[seatGrantedOffset];
+      tgm.update('seat', seatNumber);
+    }
+    if (action == AllTableState) {
+      for (int index = 2; index < 14;) {
+        SeatInfo seatInfo = SeatInfo(
+          Seat.fromInt(messageByteArray[index]),
+          messageByteArray[index++],
+          messageByteArray[index++],
+        );
+        tgm.update('seatAll', seatInfo);
+      }
+    }
   }
 
   void takeSeat(Seat seat) {
     _seat = seat;
+  }
+}
+
+class SeatInfo {
+  Seat seat = Seat.unassigned;
+  int colorIndex = 0;
+  int avatarIndex = 0;
+  SeatInfo(this.seat, this.colorIndex, this.avatarIndex);
+}
+
+enum Seat {
+  unassigned(),
+  bottom(),
+  left(),
+  top(),
+  right();
+
+  static Seat fromInt(int value) {
+    if (value == bottom) {
+      return bottom;
+    }
+    if (value == left) {
+      return left;
+    }
+    if (value == top) {
+      return top;
+    }
+    if (value == right) {
+      return right;
+    }
+    return unassigned;
   }
 }
