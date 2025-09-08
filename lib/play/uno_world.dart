c7936ca59c30c0b21a46c0ac9175e1875fc70fc2
vitalii boiko <vukelan@gmail.com>
2025-09-08 04:10:43 +0300

    check if it is player with seat

--- lib/play/uno_world.dart
+++ lib/play/uno_world.dart
@@ -14,6 +14,7 @@ import 'package:luno/player_box/button_take_seat.dart';
 import 'package:luno/player_box/player_seat.dart';
 import 'package:luno/state/table_game_manager.dart';
 import 'package:luno/state/table_state.dart';
+import 'package:luno/ws/ws_send.dart';
 
 class UnoWorld extends World {
   UnoWorld(this.tgm, {super.key});
@@ -30,10 +31,14 @@ class UnoWorld extends World {
     // add(ActiveHand());
     // add(MainPlayerBox());
     add(DeckCardsPositioned());
+
+    Future.delayed(Duration(seconds: 1), () {
+      checkPlayerSeat();
+    });
   }
 
-  double deltaTime = 0;
-  bool added = false;
+  double delayedInitializationTime = 3;
+  bool delayedInitialization = false;
 
   static const playerPositions = [
     PlayerSeat.left,
@@ -58,5 +63,20 @@ class UnoWorld extends World {
     //   }
     // }
     super.update(dt);
+  }
+}
+
+class DelayedInitialization {
+  final double delayTimeSeconds = 3;
+  double _delayedTime = 0;
+  bool isExecuted = false;
+
+  VoidCallback _callback;
+
+  DelayedInitialization(this._callback);
+
+  bool runIfDelayExpired(double time) {
+    if (isExecuted) return false;
+    return false;
   }
 }
