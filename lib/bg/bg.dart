4020264f923de6bed576f7120670451d2db20b44
vitalii boiko <vukelan@gmail.com>
2025-09-13 14:26:30 +0300

    add bg urls

---
+++ lib/bg/bg.dart
@@ -0,0 +1,13 @@
+const images = [
+  'images/bg/marek-piwnicki.jpg',
+  'images/bg/justin-luebke.jpg',
+  'images/bg/silas-baisch.jpg',
+  'images/bg/denver-saldanha.jpg',
+  'images/bg/dan-meyers.jpg',
+  'images/bg/vedant-sonani.jpg',
+  'images/bg/fabrizio-conti.jpg',
+  'images/bg/davide-longo.jpg',
+  'images/bg/lake-tahoe-boat.jpg',
+  'images/bg/marek-okon.jpg',
+  'images/bg/daniel-meyer.jpg',
+];
