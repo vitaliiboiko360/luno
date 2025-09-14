8a4a812797fee2a5186a8633f9de07fc592b5e85
vitalii boiko <vukelan@gmail.com>
2025-09-14 14:04:24 +0300

    pass screen dimentons to game

--- lib/play/game.dart
+++ lib/play/game.dart
@@ -4,8 +4,17 @@ import 'package:luno/play/uno_world.dart';
 import 'package:luno/state/table_game_manager.dart';
 
 class Game extends StatelessWidget {
-  Game(this.tgm, {super.key});
+  Game(
+    this.tgm,
+    this.screenWidth,
+    this.screenHeight,
+    this.imagePath, {
+    super.key,
+  });
 
+  double screenWidth;
+  double screenHeight;
+  String imagePath;
   TableGameManager tgm;
 
   @override
@@ -13,9 +22,28 @@ class Game extends StatelessWidget {
     return GameWidget(
       game: FlameGame(world: UnoWorld(tgm)),
       backgroundBuilder: (context) {
-        return Container(
-          decoration: BoxDecoration(color: Colors.green),
-          child: SizedBox.expand(),
+        return Stack(
+          children: [
+            Center(
+              child: Image.asset(
+                imagePath,
+                width: screenWidth,
+                height: screenHeight,
+                fit: ((screenWidth / screenHeight) * 10).toInt() > 15
+                    ? BoxFit.fitWidth
+                    : BoxFit.fitHeight,
+              ),
+            ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(color: Colors.green),
+                child: SizedBox(
+                  width: screenWidth / 2,
+                  height: screenHeight / 2,
+                ),
+              ),
+            ),
+          ],
         );
       },
     );
