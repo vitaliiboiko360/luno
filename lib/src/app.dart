962a8933687a98e8d82d0b24a355223309e8b45a
vitalii boiko <vukelan@gmail.com>
2025-09-16 00:25:45 +0300

    clean unused

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:luno/play/game.dart';
+import 'package:luno/src/game.dart';
 import 'package:luno/state/table_game_manager.dart';
-import 'package:luno/style/color_palette.dart';
-import 'package:luno/ws/ws.dart';
-import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   App(this.tgm, this.imagePath, {super.key});
@@ -19,33 +16,3 @@ class App extends StatelessWidget {
     return Game(tgm, screenWidth, screenHeight, imagePath);
   }
 }
-
-        // child: Builder(
-        //   builder: (context) {
-        //     final palette = context.watch<Palette>();
-        //     return MaterialApp.router(
-        //       title: 'Uno Game',
-        //       theme:
-        //           ThemeData.from(
-        //             colorScheme: ColorScheme.fromSeed(
-        //               seedColor: palette.darkPen,
-        //               surface: palette.backgroundMain,
-        //             ),
-        //             textTheme: TextTheme(
-        //               bodyMedium: TextStyle(color: palette.ink),
-        //             ),
-        //             useMaterial3: true,
-        //           ).copyWith(
-        //             filledButtonTheme: FilledButtonThemeData(
-        //               style: FilledButton.styleFrom(
-        //                 textStyle: const TextStyle(
-        //                   fontWeight: FontWeight.bold,
-        //                   fontSize: 20,
-        //                 ),
-        //               ),
-        //             ),
-        //           ),
-        //       routerConfig: router,
-        //     );
-        //   },
-        // ),
