715c7dcb40b36fa0b1a87a9cfa43d21b2d925595
vitalii boiko <vukelan@gmail.com>
2025-09-16 00:26:24 +0300

    move out colors

--- lib/play/play.dart
+++ lib/play/play.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:luno/style/color_palette.dart';
+import 'package:luno/colors/color_palette.dart';
 import 'package:provider/provider.dart';
 import 'dart:math';
 
