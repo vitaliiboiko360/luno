799c7a0b37f640edfdc33d7e583f426732b3adfd
vitalii boiko <vukelan@gmail.com>
2025-09-18 23:54:37 +0300

    add template for ready table

---
+++ lib/src/overlays.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class ReadyTable extends StatefulWidget {
+  ReadyTable({super.key});
+
+  @override
+  State<ReadyTable> createState() => _ReadyTableState();
+}
+
+class _ReadyTableState extends State<ReadyTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: []);
+  }
+}
