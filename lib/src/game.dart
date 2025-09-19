c4656abbe059161950302cc4c2b4aba4e52aee91
vitalii boiko <vukelan@gmail.com>
2025-09-19 03:14:44 +0300

    apply overlays

--- lib/src/game.dart
+++ lib/src/game.dart
@@ -4,6 +4,7 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:luno/bg/bg.dart';
 import 'package:luno/play/uno_world.dart';
+import 'package:luno/src/overlays.dart';
 import 'package:luno/state/table_game_manager.dart';
 
 class UnoGame extends StatelessWidget {
@@ -25,7 +26,9 @@ class UnoGame extends StatelessWidget {
     return GameWidget(
       game: Game(world: UnoWorld(tgm)),
       backgroundBuilder: buildBackground(imagePath, screenWidth, screenHeight),
-      overlayBuilderMap: {'ReadyTable': readyTable},
+      overlayBuilderMap: {
+        'ReadyTable': (BuildContext context, FlameGame game) => ReadyTable(),
+      },
     );
   }
 }
@@ -39,7 +42,9 @@ Widget readyTable(BuildContext context, FlameGame game) {
 }
 
 class Game extends FlameGame {
-  Game({super.world});
+  Game({super.world}) {
+    showPreGameOverlay();
+  }
 
   void showPreGameOverlay() {
     overlays.add('ReadyTable');
