0e6cb2d04551594d40a4d30be95d5421e01c7a3f
vitalii boiko <vukelan@gmail.com>
2025-09-21 17:37:04 +0300

    do not pass screen size

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,16 +3,12 @@ import 'package:luno/src/game.dart';
 import 'package:luno/state/table_game_manager.dart';
 
 class App extends StatelessWidget {
-  App(this.tgm, this.imagePath, {super.key});
+  App(this.tgm, {super.key});
 
   TableGameManager tgm;
-  String imagePath;
 
   @override
   Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
-    final double screenWidth = screenSize.width;
-    final double screenHeight = screenSize.height;
-    return UnoGame(tgm, screenWidth, screenHeight, imagePath);
+    return UnoGame(tgm);
   }
 }
