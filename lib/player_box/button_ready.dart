2adae66ddfa9be839c28449355072a4e0454df26
vitalii boiko <vukelan@gmail.com>
2025-09-24 00:54:45 +0300

    improve button style

--- lib/player_box/button_ready.dart
+++ lib/player_box/button_ready.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'dart:ui' as ui;
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:luno/player_box/position.dart';
@@ -38,11 +38,16 @@ class PainterButtonReady extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     var strokePaint = Paint()
-      ..color = Colors.lime
+      ..color = const ui.Color.fromARGB(255, 122, 134, 9)
+      ..strokeWidth = 3
       ..style = PaintingStyle.stroke;
     var roundedRect = RRect.fromLTRBR(0, 0, 150, 100, Radius.circular(50));
     canvas.drawRRect(roundedRect, strokePaint);
     var fillPaint = Paint()
+      ..shader = ui.Gradient.radial(Offset(75, 0), 100, [
+        Colors.green,
+        Colors.lime,
+      ])
       ..color = Colors.lightGreen
       ..style = PaintingStyle.fill;
     canvas.drawRRect(roundedRect, fillPaint);
