fe1ea9ed9ff6cee81bc52c36f2f08a90cf5ab50d
vitalii boiko <vukelan@gmail.com>
2025-10-01 02:39:16 +0300

    use ProviderScope instead of provider contianer

--- lib/src/game.dart
+++ lib/src/game.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:luno/bg/bg.dart';
 import 'package:luno/play/uno_world.dart';
 import 'package:luno/src/overlay_playerconfig.dart';
@@ -21,12 +22,12 @@ class UnoGame extends StatelessWidget {
       overlayBuilderMap: {
         'ReadyTable': (BuildContext context, Game game) => ReadyTable(),
         'Anouncement': (BuildContext context, Game game) => Anouncement(),
-        // 'PlayerConfig': (BuildContext context, Game game) =>
-        //     UncontrolledProviderScope(
-        //       container: tgm.providerContainer,
-        //       child: PlayerConfig(tgm),
-        //     ),
-        'PlayerConfig': (BuildContext context, Game game) => PlayerConfig(tgm),
+        'PlayerConfig': (BuildContext context, Game game) =>
+            UncontrolledProviderScope(
+              container: tgm.providerContainer,
+              child: PlayerConfig(tgm),
+            ),
+        // 'PlayerConfig': (BuildContext context, Game game) => PlayerConfig(tgm),
       },
       initialActiveOverlays: ['ReadyTable', 'PlayerConfig'],
     );
