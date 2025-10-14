31530e586d06225de4844339f6b2eee9eb4a9ab8
vitalii boiko <vukelan@gmail.com>
2025-10-14 03:40:55 +0300

    add new events for change buttons

--- lib/state/events.dart
+++ lib/state/events.dart
@@ -2,6 +2,8 @@ enum Event {
   seat('seat'),
   seatAll('seatAll'),
   avatar('avatar'),
+  addChangeButton('addChangeButton'),
+  removeChangeButton('removeChangeButton'),
   end('end');
 
   final String name;
